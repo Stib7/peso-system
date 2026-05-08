1af9db5932eff3351568cd396f4bdf1c98cee482
John Steven T. Buhian <johnstevenbuhian@gmail.com>
2026-05-08 15:47:10 +0800

    prog user create

--- admin/programs/program-create.php
+++ admin/programs/program-create.php
@@ -1,4 +1,4 @@
-<?php include('includes/header.php') ?>
+<?php include('../includes/header.php') ?>
 
     <div class="container-fluid">    
         
@@ -71,4 +71,4 @@
 
 
     </div>
-<?php include('includes/footer.php') ?>
+<?php include('../includes/footer.php') ?>
--- admin/users/users-create.php
+++ admin/users/users-create.php
@@ -73,4 +73,4 @@
 
 
     </div>
-<?php include('./includes/footer.php') ?>
+<?php include('../includes/footer.php') ?>
